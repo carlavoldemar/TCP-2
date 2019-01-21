6da5afcd5c73e4cfab979b51b341160f89c76a3f
carlavoldemar <carlavoldemar@gmail.com>
2019-01-21 10:30:48 -0200

    versao 2

---
+++ DivideDoisNum.java
@@ -0,0 +1,14 @@
+/*
+ * Escreva um programa para imprimir a divisão de dois números.
+ */
+public class DivideDoisNum {
+
+	public static void main(String[] args) {
+		int primeiro = 300;
+		int segundo = 30;
+		
+		System.out.println("A divisão de dois números é: " + (primeiro / segundo));
+
+	}
+
+}
